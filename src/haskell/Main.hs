d278749c6d7b6992db688bfeeca46a0920a0da9f
ianacarr <143140156+ianacarr@users.noreply.github.com>
2026-09-16 16:00:26 -0400

    FFI still not working

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -10,6 +10,7 @@ foreign export ccall "haskell_add" add :: CInt -> CInt -> CInt
 
 add :: CInt -> CInt -> CInt
 add x y = x + y
+{-# NOINLINE add #-}
 
 pipeline :: String -> String
 pipeline e = DR.getPath e
