fe01e88a739a99b73bf40bb2f63069a9f4a50f6e
ianacarr <143140156+ianacarr@users.noreply.github.com>
2026-09-16 15:40:20 -0400

    (not working) Haskell FFI

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 module Main where
 
 import Data.Retrieval as DR
+
+import Foreign.C.Types
+
+foreign export ccall "haskell_add" add :: CInt -> CInt -> CInt
+
+add :: CInt -> CInt -> CInt
+add x y = x + y
 
 pipeline :: String -> String
 pipeline e = DR.getPath e
 
 main :: IO ()
 main = putStrLn "hello world!"
-
