fa533147fb9273ea16c2033a8a8f31d12a409c1e
duncnar <duncn.ritchie@gmail.com>
2026-09-20 13:54:29 -0400

    corrected cabal file, worked on API data retrieval

--- src/haskell/Data/Retrieval.hs
+++ src/haskell/Data/Retrieval.hs
@@ -1,15 +1,30 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.Retrieval (pipe) where
 
 import Network.HTTP.Req
+import qualified Data.Text as T
 -- import Control.Lens (view)
--- import Data.Aeson.Lens (key, _String)
--- import qualified Data.ByteString.Lazy.Char8 as BL
+-- import Data.Aeson
+import qualified Data.ByteString.Lazy as BS
 
-pipe :: String -> Url
-pipe = getPath e
+pipe :: String -> IO BS.ByteString
+pipe = get_data . get_RCSB_path 
 
-getPath :: String -> Url 
-getPath e = https "rcsb.org" /: "rest" /: "v1" /: "core" /: "entry" /: e
+get_RCSB_path :: String -> Url Https
+get_RCSB_path eID = https 
+   "files.rcsb.org" /: 
+   "pub" /: 
+   "pdb" /: 
+   "data" /:
+   "structures" /: 
+   "all" /:
+   "mmCIF" /:
+   T.append (T.pack eID) ".cif.gz"
 
-
+get_data :: Url Https -> IO BS.ByteString
+get_data path = runReq defaultHttpConfig $ do 
+   r <- req GET path NoReqBody lbsResponse mempty
+   pure (responseBody r)
 
--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -2,22 +2,26 @@
 
 module Main where
 
-import Data.Retrieval as DR
-
+import Data.Retrieval as R
+import Data.Aeson
 import Foreign.C.Types
+import qualified Data.ByteString.Lazy as BS
 
+{-
 foreign export ccall "haskell_add" add :: CInt -> CInt -> CInt
 
 add :: CInt -> CInt -> CInt
 add x y = x + y
 {-# NOINLINE add #-}
+-}
 
-pipeline :: String -> String
-pipeline e = DR.pipe e
+pipeline :: String -> IO BS.ByteString
+pipeline e = R.pipe e
 
 main :: IO ()
 main = do
    putStrLn "Enter RCSB protein ID: "
    id <- getLine
-   putStrLn (pipeline id)
+   f <- pipeline id
+   BS.writeFile ("./Test/" ++ id ++ ".txt") f
 
